4685b7e560f31e57b075e33594e265eb3560fe3e
Michał Nowotka <mmmnow@gmail.com>
2018-11-17 21:11:57 +0100

    Update PangramChecker.java

--- src/main/java/PangramChecker.java
+++ src/main/java/PangramChecker.java
@@ -3,19 +3,8 @@ import java.util.Set;
 
 public class PangramChecker {
 
-    private static final Set<Character> ALPHABET = new HashSet<>();
-    static {
-        for (char c: "abcdefghijklmnopqrstuvwxyz".toCharArray()) {
-            ALPHABET.add(c);
-        }
-    }
-
     public boolean isPangram(String input) {
-        Set<Character> in = new HashSet<>();
-        for (char c: input.toLowerCase().toCharArray()){
-            in.add(c);
-        }
-        return in.containsAll(ALPHABET);
+        throw new UnsupportedOperationException("Delete this line and write your own implementation");
     }
 
 }
