2c4bb196b6979b9d6ee10931bb8c9447b5a0b14a
Michał Nowotka <mmmnow@gmail.com>
2018-11-17 21:13:20 +0100

    Update PangramCheckerTest.java

--- src/test/java/PangramCheckerTest.java
+++ src/test/java/PangramCheckerTest.java
@@ -20,54 +20,62 @@ public class PangramCheckerTest {
     }
 
 
+    @Ignore
     @Test
     public void recognizesPerfectLowerCasePangram() {
         assertTrue(pangramChecker.isPangram("abcdefghijklmnopqrstuvwxyz"));
     }
 
 
+    @Ignore
     @Test
     public void pangramWithOnlyLowerCaseLettersIsRecognizedAsPangram() {
         assertTrue(pangramChecker.isPangram("the quick brown fox jumps over the lazy dog"));
     }
 
 
+    @Ignore
     @Test
     public void phraseMissingCharacterXIsNotPangram() {
         assertFalse(pangramChecker.isPangram("a quick movement of the enemy will jeopardize five gunboats"));
     }
 
 
+    @Ignore
     @Test
     public void phraseMissingAnotherCharacterIsNotPangram() {
         assertFalse(pangramChecker.isPangram("five boxing wizards jump quickly at it"));
     }
 
 
+    @Ignore
     @Test
     public void pangramWithUnderscoresIsRecognizedAsPangram() {
         assertTrue(pangramChecker.isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog"));
     }
 
 
+    @Ignore
     @Test
     public void pangramWithNumbersIsRecognizedAsPangram() {
         assertTrue(pangramChecker.isPangram("the 1 quick brown fox jumps over the 2 lazy dogs"));
     }
 
 
+    @Ignore
     @Test
     public void phraseWithMissingLettersReplacedByNumbersIsNotPangram() {
         assertFalse(pangramChecker.isPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"));
     }
 
-
+    @Ignore
     @Test
     public void pangramWithMixedCaseAndPunctuationIsRecognizedAsPangram() {
         assertTrue(pangramChecker.isPangram("\"Five quacking Zephyrs jolt my wax bed.\""));
     }
 
 
+    @Ignore
     @Test
     public void upperAndLowerCaseVersionsOfTheSameCharacterShouldNotBeCountedSeparately() {
         assertFalse(pangramChecker.isPangram("the quick brown fox jumps over with lazy FX"));
