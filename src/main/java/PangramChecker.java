7545859e44d99a01177037bd5e17dfbbbd092a85
nickl0gist <nicklogistick@mail.ru>
2019-06-15 13:20:17 +0200

    update

--- src/main/java/PangramChecker.java
+++ src/main/java/PangramChecker.java
@@ -4,7 +4,6 @@ import java.util.Set;
 public class PangramChecker {
 
     public boolean isPangram(String input) {
-        boolean res = true;
         input = input.toLowerCase();
         Set<Character> set = new HashSet<>();
         for (int i = 0; i < input.length(); i++) {
